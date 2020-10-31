60bbbe05ddede2c22f545842563c4a25fad9820b
yuniel <yunielrc87@gmail.com>
2020-10-30 23:39:39 -0400

    add openvpn-shadowsocks-proxy

---
+++ openvpn-shadowsocks-proxy/main.tf
@@ -0,0 +1,155 @@
+locals {
+  user              = "ubuntu"
+  cidr_blocks       = ["0.0.0.0/0"]
+  sufix             = formatdate("YYYYMMDDhhmmss", timestamp())
+  provision_home    = "./provision/home"
+  provision_root    = "./provision/root"
+  provision_scripts = "./provision/scripts"
+}
+
+provider "aws" {
+  profile = var.profile
+  region  = var.region
+  # access_key = var.aws_access_key
+  # secret_key = var.aws_secret_key
+}
+
+resource "aws_key_pair" "key_pair" {
+  key_name   = "${var.name}-key-pair-${local.sufix}"
+  public_key = file("~/.ssh/id_rsa.pub")
+  # public_key = var.public_key
+}
+
+resource "aws_security_group" "security_group" {
+  name = "${var.name}-security-group-${local.sufix}"
+
+  ingress {
+    description = "ss server tcp"
+    from_port   = var.ss_port
+    to_port     = var.ss_port
+    protocol    = "tcp"
+    cidr_blocks = local.cidr_blocks
+  }
+
+  ingress {
+    description = "http proxy"
+    from_port   = var.squid_port
+    to_port     = var.squid_port
+    protocol    = "tcp"
+    cidr_blocks = local.cidr_blocks
+  }
+
+  ingress {
+    description = "icmp"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = local.cidr_blocks
+  }
+
+  ingress {
+    description = "ssh"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = local.cidr_blocks
+  }
+
+  egress {
+    description = "outgoing"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = local.cidr_blocks
+  }
+}
+
+resource "aws_instance" "instance" {
+  disable_api_termination = var.disable_termination
+  ami                     = var.amis[var.region] # us-east-2 ubuntu 20.04 x64
+  instance_type           = var.instance_type
+  vpc_security_group_ids  = [aws_security_group.security_group.id]
+  key_name                = aws_key_pair.key_pair.id
+
+  tags = {
+    Name = var.name
+  }
+
+  connection {
+    type    = "ssh"
+    host    = self.public_ip
+    user    = local.user
+    timeout = "1m"
+    agent   = true
+  }
+
+  # PROVISION
+
+  ## enable ssh root login
+  provisioner "remote-exec" {
+    script = "${local.provision_scripts}/enable-ssh-root"
+  }
+
+  ## upload home data
+  provisioner "file" {
+    source      = "${local.provision_home}/"
+    destination = "/home/${local.user}"
+  }
+
+  ## upload root data
+  provisioner "file" {
+    source      = "${local.provision_root}/"
+    destination = "/"
+
+    connection {
+      type  = "ssh"
+      host  = self.public_ip
+      user  = "root"
+      agent = true
+    }
+  }
+
+  ## upload scripts
+  provisioner "file" {
+    source      = local.provision_scripts
+    destination = "/tmp"
+  }
+
+  ## setup docker
+  provisioner "remote-exec" {
+    inline = ["wget -qO - https://git.io/JJaKZ?=docker-ubuntu | bash"]
+  }
+
+  # setup squid, openvpn-ssserver (shadowsocks-rust server)
+  provisioner "remote-exec" {
+    inline = [
+      <<-EOT
+        wget -qO - https://git.io/JT9Gz?=openvpn-shadowsocks-server-linux |
+          WORKING_DIR=/home/${local.user}/openvpn-ssserver \
+          SS_CLIENT_PORT=${var.ss_client_port} \
+          SS_PORT=${var.ss_port} \
+          SS_PASSWORD=${var.ss_password} \
+          OPENVPN_CLIENT_NAME=${var.openvpn_client_name} \
+          bash
+      EOT
+      ,
+      <<-EOT
+        chmod +x /tmp/scripts/setup-squid
+        SQUID_PORT=${var.squid_port} \
+        SQUID_USER=${var.squid_user} \
+        SQUID_PASSWORD=${var.squid_password} \
+        /tmp/scripts/setup-squid
+      EOT
+      ,
+    ]
+  }
+
+  # add server public keys to local known hosts
+  # copy vpn profile from server to local
+  provisioner "local-exec" {
+    command = <<-EOT
+      ssh-keyscan -T 120 ${self.public_ip} >> ~/.ssh/known_hosts
+      scp ${local.user}@${self.public_ip}:~/openvpn-ssserver/${var.openvpn_client_name}.ovpn ~/
+    EOT
+  }
+}
---
+++ openvpn-shadowsocks-proxy/variables.tf
@@ -0,0 +1,93 @@
+variable "name" {
+  type        = string
+  description = "project name, resources prefix, instance tag Name"
+}
+
+variable "region" {
+  type        = string
+  default     = "us-east-2"
+  description = "aws instance region"
+}
+
+variable "profile" {
+  type        = string
+  default     = "default"
+  description = "aws profile"
+}
+
+variable "instance_type" {
+  type        = string
+  default     = "t2.micro"
+  description = "instance type"
+}
+
+variable "amis" {
+  type = map(string)
+  default = {
+    us-east-2 = "ami-01237fce26136c8cc"
+  }
+  description = "aws amazon machine images"
+}
+
+variable "disable_termination" {
+  type = bool
+  description = "disable instance termination"
+  default = false
+}
+
+# variable "public_key" {
+#   type = string
+# }
+
+# variable "aws_access_key" {
+#   type = string
+# }
+
+# variable "aws_secret_key" {
+#   type = string
+# }
+
+# SQUID
+
+variable "squid_port" {
+  type        = number
+  default     = 3128
+  description = "squid port"
+}
+
+variable "squid_user" {
+  type        = string
+  description = "squid user"
+}
+
+variable "squid_password" {
+  type        = string
+  description = "squid password"
+}
+
+# OPENVPN
+
+variable "openvpn_client_name" {
+  type        = string
+  default     = "profile"
+  description = "openvpn profile"
+}
+
+# SHADOWSOCKS (SS) SERVER
+
+variable "ss_client_port" {
+  type        = number
+  default     = 1080
+  description = "shadowsocks client port"
+}
+
+variable "ss_port" {
+  type        = number
+  default     = 443
+  description = "shadowsocks server port"
+}
+
+variable "ss_password" {
+  type        = string
+  description = "shadowsocks password"
+}
